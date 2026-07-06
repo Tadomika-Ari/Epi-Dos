fbb28ea07431d6bfb57e4127eba83048cfa5cac7
Lucas <lucas1.eeckhoutte@epitech.eu>
2026-07-06 19:52:15 +0200

    feat!: add main file for ctOS

---
+++ ctOS.py
@@ -0,0 +1,30 @@
+from src.ctOS.start.init import demo
+from src.ctOS.start.start import start
+from src.ctOS.help import help
+import sys
+from dotenv import load_dotenv
+from src.ctOS.music.music import music_command
+from src.ctOS.music.init_token import init
+load_dotenv()
+
+def main():
+    nb = len(sys.argv)
+    
+    if nb <= 1:
+        help()
+        return
+    if sys.argv[1] == "--music":
+        music_command()
+        return
+    if sys.argv[1] == "--init":
+        init()
+        return
+    if sys.argv[1] == "--start":
+        demo()
+        start()
+        return
+    if sys.argv[1] == "--help" or "-h":
+        help()
+    else:
+        return 84
+main()
