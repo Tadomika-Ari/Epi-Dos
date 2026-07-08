72181bf193f498d7ebc83ac0a85b0981797a35b0
Lucas <lucas1.eeckhoutte@epitech.eu>
2026-07-08 15:23:03 +0200

    feat!: add neccesery for use program

--- src/tranquillity.py
+++ src/tranquillity.py
@@ -6,29 +6,29 @@ from src.core.core import core
 from src.core.waiting_message import waiting_message
 from src.core.fast import main as llm_fast
 
-def init():
-    print("\n")
-    print("Bonjour utilisateur\n")
-    print("Que voulez vous faire ?\n")
-    print("1 : llm with voice.\n")
-    print("2 : llm with context and voice.\n")
-    print("3 : with threading\n")
-    print("4 : waiting message\n")
-    print("5 : fast message")
-    choice = input("donne ton choix : ")
+def init(num: int):
+    if (num != 5):
+        print("\n")
+        print("Bonjour utilisateur\n")
+        print("Que voulez vous faire ?\n")
+        print("1 : llm with voice.\n")
+        print("2 : llm with context and voice.\n")
+        print("3 : with threading\n")
+        print("4 : waiting message\n")
+        print("5 : fast message")
+        choice = input("donne ton choix : ")
 
-    if (int(choice) == 1):
-        llm()
-    if (int(choice) == 2):
-        llm_with_contexte()
-    if (int(choice) == 3):
-        core()
-    if (int(choice) == 4):
-        waiting_message()
-    if (int(choice) == 5):
+        if (int(choice) == 1):
+            llm()
+        if (int(choice) == 2):
+            llm_with_contexte()
+        if (int(choice) == 3):
+            core()
+        if (int(choice) == 4):
+            waiting_message()
+        if (int(choice) == 5 or num == 5):
+            llm_fast()
+    if (num == 5):
         llm_fast()
     if (int(choice) != 0):
         return
-
-if __name__ == "__main__":
-    init()
